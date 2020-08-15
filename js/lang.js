224b0a9310cc89e4363cde671bef65f3a72d6d9e
tadasan <tadand2@ktu.edu>
2020-08-16 02:11:21 +0300

    adding owrking translations with js and cms

--- js/lang.js
+++ js/lang.js
@@ -1,54 +1,24 @@
-var LanguageList = {
-    "LT" : "LT /",
-    "EN" : "EN"
-  };
-
-//  window.onload = initializeTexts
-
-//  function initializeTexts() {
-//     $.getScript("../content/cms.json", function(res) {
-//       translations = JSON.parse(res)
-//       if (translations) {
-//         Object.keys(translations).forEach(tr => {
-//           $("." + tr).html(translations[tr])
-//         })
-//       }
-//     })
-//  }
-
-  var WORDS_LT = {
-    text1: "tekstas Vienas",
-    text2: "tekstas Du",
-    heading1: "Ką mes veikiame?"
-  };
-
-  var WORDS_EN = {
-    text1: "text One",
-    text2: "text Two",
-    heading1: "What we do?"
-  };
-  
-  
-  
- window.onload = initialize;
-  
-  function initialize() {
-    var $dropdown = $("#country_select");    
-    $.each(LanguageList, function(key, value) {
-      $dropdown.
-        append($("<option/>").
-        val(key).
-        text(value));
-      });
-      
-    loadsLanguage("LT");
+  var languageList = {
+    "lt": "lt",
+    "en": "en",
+    "ru": "ru"
   }
-  
-  function loadsLanguage(lang){
-    /*fills all the span tags with class=lang pattern*/ 
-    $('span[class^="lang"]').each(function(){
-      var LangVar = (this.className).replace('lang-','');
-      var Text = window["WORDS_"+lang][LangVar];
-      $(this).text(Text);        
-    });
+
+  window.onload = setTexts()
+
+  $('#country_select').change(() => setTexts())
+
+  $('#a_country_select').click((e) => e.preventDefault())
+
+  function setTexts(e) {
+    const lang = $("#country_select option:selected").val()
+
+    $.getScript("../content/cms.json", (res) => {
+      const texts = JSON.parse(res)
+      if (texts) {
+        Object.keys(texts).forEach(t => {
+          $("." + t).html(texts[t][lang])
+        })
+      }
+    })
   }
