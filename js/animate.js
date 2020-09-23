60b5a66de9cda97e3a74addfc4df3e123026f42f
mildaslepaviciute <milda.slepaviciute@gmail.com>
2020-09-23 23:37:16 +0300

    change

--- js/animate.js
+++ js/animate.js
@@ -3,29 +3,41 @@ $(document).ready(function() {
     $('.js--wp-1').waypoint(function(direction) {
       $('.js--wp-1').addClass('animated fadeIn');
     }, {
-      offset: '90%'
-    })  
-  
+      offset: '100%'
+    })
+    
     $('.js--wp-2').waypoint(function(direction) {
-      $('.js--wp-2').addClass('animated backInRight');
+      $('.js--wp-2').addClass('animated fadeIn');
     }, {
-      offset: '80%'
+      offset: '100%'
     })
 
     $('.js--wp-3').waypoint(function(direction) {
-      $('.js--wp-3').addClass('animated fadeInLeft');
+      $('.js--wp-3').addClass('animated fadeIn');
     }, {
       offset: '100%'
     })
 
     $('.js--wp-4').waypoint(function(direction) {
-      $('.js--wp-4').addClass('animated fadeInRight');
+      $('.js--wp-4').addClass('animated fadeIn');
     }, {
       offset: '100%'
     })
 
     $('.js--wp-5').waypoint(function(direction) {
-      $('.js--wp-5').addClass('animated fadeInUp');
+      $('.js--wp-5').addClass('animated fadeIn');
+    }, {
+      offset: '100%'
+    })
+
+    $('.js--wp-6').waypoint(function(direction) {
+      $('.js--wp-6').addClass('animated fadeIn');
+    }, {
+      offset: '100%'
+    })
+
+    $('.js--wp-7').waypoint(function(direction) {
+      $('.js--wp-7').addClass('animated fadeIn');
     }, {
       offset: '100%'
     })
