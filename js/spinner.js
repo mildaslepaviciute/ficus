f2c4a90dd374a5e137dedc91cfc754e43c56d69e
mildaslepaviciute <milda.slepaviciute@gmail.com>
2020-09-24 15:13:22 +0300

    change

--- js/spinner.js
+++
@@ -1,8 +0,0 @@
-// function show () {
-//     document.getElementById("spinner-front").classList.add("show");
-//     document.getElementById("spinner-back").classList.add("show");
-//   }
-//   function hide () {
-//     document.getElementById("spinner-front").classList.remove("show");
-//     document.getElementById("spinner-back").classList.remove("show");
-//   }
